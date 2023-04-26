92d3bed57ec61ebc914dc2c186752c085ea81ee3
Lina Datkunaite <l.datkunaite@gmail.com>
2023-04-26 21:58:39 +0300

    add migrations to git ignore, add admin site personalizations, add new models

--- carservice/admin.py
+++ carservice/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CarModel, Car, Service, ServicePrice, Order
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('car', 'service', 'quantity', 'order_date', )
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('client', 'car_model',  'plate_nr', 'vin_number')
+    search_fields = ('plate_nr', 'vin_number', 'client')
+    list_filter = ('client', 'car_model')
+
+admin.site.register(CarModel)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Service)
+admin.site.register(ServicePrice)
+admin.site.register(Order, OrderAdmin)
--- carservice/models.py
+++ carservice/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 import datetime
 class CarModel(models.Model):
     car_model_id = models.AutoField(primary_key=True)
@@ -8,16 +9,26 @@ class CarModel(models.Model):
     engine = models.CharField('Engine', max_length=100)
     def __str__(self):
         return f'{self.brand} - {self.car_model}'
+
+    class Meta:
+        verbose_name = 'Car Model'
+        verbose_name_plural = 'Car Models'
+
+
+
 class Car(models.Model):
     car_id = models.AutoField(primary_key=True)
-    car_model = models.ForeignKey(CarModel, on_delete=models.SET.NULL, null=True)
+    car_model = models.ForeignKey(CarModel, on_delete=models.SET_NULL, null=True)
     plate_nr = models.CharField('Plate nr', max_length=20)
-    vin_number = models.CharField('VIN', max_length=200)
+    vin_number = models.CharField('VIN', max_length=17)
     client = models.CharField('Client', max_length=100)
 
     def __str__(self):
         return f'{self.client} - {self.car_model} - {self.plate_nr}'
 
+    class Meta:
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
 class Service(models.Model):
     service_id = models.AutoField(primary_key=True)
     service_name = models.CharField('Service', max_length=500)
@@ -25,32 +36,32 @@ class Service(models.Model):
     def __str__(self):
         return f'{self.service_name}'
 
-
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
 class ServicePrice(models.Model):
     service_price_id = models.AutoField(primary_key=True)
-    service = models.ForeignKey(Service, on_delete=models.SET_NULL(), null=True)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
     cars = models.ManyToManyField(CarModel)
     price = models.FloatField("Price")
 
     def __str__(self):
         return f'{self.service} - {self.price}'
 
-
+    class Meta:
+        verbose_name = 'Service Price'
+        verbose_name_plural = 'Service Prices'
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
-    order_date = models.DateTimeField(default=datetime.datetime.now())
-    car = models.ForeignKey(Car, on_delete=models.SET_NULL(), null=True)
+    order_date = models.DateTimeField(default=timezone.now)
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
     total_price = models.FloatField()
 
     def __str__(self):
-        return f'{self.car} - {self.total_price} - {self.order_date}'
+        return f'{self.car} - {self.service} - {self.total_price} - {self.order_date}'
 
-class OrderList(models.Model):
-    order_list_id = models.AutoField(primary_key=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField()
-    price = models.FloatField()
-
-    def __str__(self):
-        return f'{self.service} - {self.quantity} - {self.price}'
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
